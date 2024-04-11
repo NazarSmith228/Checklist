3109cf0b9eb04d26aded25b801c7336673a69c03
Safarov Renat <SafarovRenat1995@gmail.com>
2024-04-11 08:01:36 +0300

    * saveList() method

--- src/main/java/com/bobocode/checklist/entity/CheckList.java
+++ src/main/java/com/bobocode/checklist/entity/CheckList.java
@@ -1,11 +1,16 @@
 package com.bobocode.checklist.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CheckList {
     private Long id;
+    private String title;
     private List<Task> tasks;
 }
--- src/main/java/com/bobocode/checklist/service/ChecklistService.java
+++ src/main/java/com/bobocode/checklist/service/ChecklistService.java
@@ -5,9 +5,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class ChecklistService {
 
     private final Map<Long, CheckList> taskListByIdMap = new ConcurrentHashMap<>();
+    private final AtomicLong listIdGenerator = new AtomicLong(0L);
+    private final AtomicLong taskIdGenerator = new AtomicLong(0L);
+
+    public CheckList saveList(CheckList checkList) {
+        Long generatedId = listIdGenerator.incrementAndGet();
+        checkList.getTasks()
+                .forEach(task -> task.setId(taskIdGenerator.incrementAndGet()));
+        taskListByIdMap.put(generatedId, checkList);
+
+        return checkList;
+    }
 }
